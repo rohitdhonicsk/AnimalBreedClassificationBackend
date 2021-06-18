17164cee8d403e315e72a80d7fa1f06159a28e18
Rohit Kumar Gupta <rohitgupta.dbs@gmail.com>
2021-06-19 02:30:53 +0530

    created Form For Adding Species

--- app.py
+++ app.py
@@ -7,21 +7,87 @@ import time
 from flask import redirect
 
 app = Flask(__name__)
-
-
-Animal = os.listdir("Data")
 AnimalKeyJson = {}
-for animal in Animal:
-    AnimalKeyJson[animal[:-5]] = json.load(open("Data/" + animal))
-# Case Insensitive lowercase + strip key
-AnimalKeyJson = dict(
-    map(lambda keyv: ((keyv[0].lower()).strip(), keyv[1]), AnimalKeyJson.items())
-)
+
+
+def read():
+    Animal = os.listdir("Data")
+    global AnimalKeyJson
+    for animal in Animal:
+        try:
+            AnimalKeyJson[animal[:-5]] = json.load(open("Data/" + animal))
+        except:
+            pass
+    # Case Insensitive lowercase + strip key
+    AnimalKeyJson = dict(
+        map(lambda keyv: ((keyv[0].lower()).strip(), keyv[1]), AnimalKeyJson.items())
+    )
+
+
+read()
 
 
 @app.route("/all")
 def printAll():
     return AnimalKeyJson
+
+
+# # data = request.json
+#         data = request.get_json()
+#         try:
+#             print(request.data, "asdf", request.values)
+#             try:
+#                 print(request.get_data())
+#                 print(request.values.get("target"))
+#             except:
+#                 pass
+#         except:
+#             pass
+
+#         ff = request.form
+#     if request.method == "POST":
+#         data = request.json
+#         ff = request.form
+#         dd = request.data
+#         print(data, ff, dd)
+#     return "hello"
+
+
+@app.route("/AddData", methods=["GET", "POST"])
+def AddDetail():
+    ExistAnimal = [x[:-5] for x in os.listdir("Data/")]
+    if request.method == "POST":
+        data = request.json
+        print(data)
+        dic = {}
+        try:
+            dic["Format"] = data["Format"]
+        except:
+            dic = data["Breed"]
+
+        fileName = data["AnimalName"]
+        if os.path.exists("Data/" + fileName + ".json"):
+            mode = "r+"  # append if already exists
+        else:
+            mode = "w"
+        with open("Data/" + fileName + ".json", mode) as fp:
+            try:
+                file_data = json.load(fp)
+            except:
+                file_data = {}
+            keys = tuple(dic.keys())[0]
+            file_data[keys] = dic[keys]
+            fp.seek(0)
+            print(file_data)
+            json.dump(file_data, fp, indent=4)
+        read()
+
+        # # dd = request.data
+        # print("asd")
+        return "done"
+    else:
+        # return "dsd"
+        return render_template("form.html", ExistAnimal=ExistAnimal)
 
 
 @app.route("/animal/<animal>")
